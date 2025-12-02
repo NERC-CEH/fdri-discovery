dc5d80d987f2271f84207fc147891390677de0de
Kal Ahmed <kal.ahmed@epimorphics.com>
2025-12-02 12:18:02 +0000

    Remove obsolete SQL file

--- sample_data/sql/sontek_sites.sql
+++
@@ -1,8 +0,0 @@
-COPY(
-    SELECT
-        "Site Name",
-        "Site Number",
-        "Latitude",
-        "Longitude"
-    FROM './sample_data/src/sontek/sites.parquet'
-) TO './build/sontek_sites.csv';
