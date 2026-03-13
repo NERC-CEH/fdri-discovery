966b54601a3347cb0d9224920e4195e0fc1ba26c
Simon Stanley <simsta@ceh.ac.uk>
2026-03-13 16:23:58 +0000

    Add diagonostic

--- sample_data/sql/instrumentation_parameters.sql
+++ sample_data/sql/instrumentation_parameters.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS TS AS FROM read_csv('./sample_data/src/TIMESERIES_IDS_COSMOS.csv', AUTO_DETECT=true, normalize_names=true);
 CREATE TABLE IF NOT EXISTS MEASURE AS FROM read_csv('./sample_data/src/MEASURES.csv', AUTO_DETECT=true, normalize_names=true);
 CREATE TABLE IF NOT EXISTS SI AS FROM read_csv('./sample_data/src/SITE_INSTRUMENTATION.csv', AUTO_DETECT=true, normalize_names=true);
+
+-- Diagnostic: print actual column names
+SELECT column_name FROM information_schema.columns WHERE table_name = 'SI';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'TS';
+
 COPY (
     SELECT DISTINCT MEASURE.parameter_id, SI.instrument_id
     FROM TS JOIN MEASURE ON TS.measure_id == MEASURE.id 
