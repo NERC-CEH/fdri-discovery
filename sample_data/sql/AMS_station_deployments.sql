17c0b8bf1db5bca6c63f6e1e50a430a303b65642
Kal Ahmed <kal.ahmed@epimorphics.com>
2026-03-16 16:29:35 +0000

    Add test site * Add a row to AMS_sites for the Wallingford test site * Add join logic to ensure that only deployments of assets listed in AMS_asset.csv are included in the output

--- sample_data/sql/AMS_station_deployments.sql
+++ sample_data/sql/AMS_station_deployments.sql
@@ -1,5 +1,6 @@
 create table sites as from read_csv('./sample_data/src/AMS_sites.csv', AUTO_DETECT=true);
 create table lh as from read_csv('./sample_data/src/AMS_asset_location_history.csv', AUTO_DETECT=true);
+create table assets as from read_csv('./sample_data/src/AMS_asset.csv', AUTO_DETECT=true);
 create table lhx as select *,
     strftime(locHistory_Date, '%Y-%m-%dT%H:%M:%SZ') as start_date,
     strftime((
@@ -13,5 +14,5 @@ COPY(
         sites.site_StationID,
         sites.site_Network,
         lhx.*
-    from sites join lhx on sites.site_StationID = lhx.locHistory_SiteCode
+    from sites join lhx on sites.site_StationID = lhx.locHistory_SiteCode join assets on lhx.locHistory_AssetID = assets.ID
 ) TO './build/AMS_station_deployments.csv' (HEADER, DELIMITER ',') ;
