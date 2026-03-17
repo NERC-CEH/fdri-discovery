2476643cb7f48bdcafefc8bb6ae67c328a9f3b5f
Simon Stanley <simsta@ceh.ac.uk>
2026-03-17 11:25:56 +0000

    Remove semi-colon and explicit seperator

--- sample_data/sql/instrumentation_parameters.sql
+++ sample_data/sql/instrumentation_parameters.sql
@@ -1,6 +1,11 @@
 CREATE TABLE IF NOT EXISTS TS AS FROM read_csv('./sample_data/src/TIMESERIES_IDS_COSMOS.csv', AUTO_DETECT=true);
 CREATE TABLE IF NOT EXISTS MEASURE AS FROM read_csv('./sample_data/src/MEASURES.csv', AUTO_DETECT=true);
-CREATE TABLE IF NOT EXISTS SI AS FROM read_csv('./sample_data/src/SITE_INSTRUMENTATION.csv', AUTO_DETECT=true);
+CREATE TABLE IF NOT EXISTS SI AS FROM read_csv('./sample_data/src/SITE_INSTRUMENTATION.csv', sep=',');
+
+-- Diagnostic: print actual column names
+SELECT column_name FROM information_schema.columns WHERE table_name = 'SI';
+SELECT column_name FROM information_schema.columns WHERE table_name = 'TS';
+
 COPY (
     SELECT DISTINCT MEASURE.PARAMETER_ID, SI.INSTRUMENT_ID
     FROM TS JOIN MEASURE ON TS.MEASURE_ID == MEASURE.ID JOIN SI on TS.SENSOR_SLOT_ID == SI.SENSOR_SLOT_ID AND TS.SITE_ID == SI.SITE_ID
