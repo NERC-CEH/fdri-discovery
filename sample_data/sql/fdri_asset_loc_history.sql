d9cd55647244aa4449783c2ce6700a0d574369e4
Simon Stanley <simsta@ceh.ac.uk>
2026-06-11 10:04:42 +0100

    Correct column names

--- sample_data/sql/fdri_asset_loc_history.sql
+++ sample_data/sql/fdri_asset_loc_history.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS SITES AS FROM read_csv('./sample_data/src/SITES_FDRI.csv', AUTO_DETECT=true);
 CREATE TABLE IF NOT EXISTS HISTORY AS FROM read_csv('./sample_data/src/fdri_loc_history.csv', AUTO_DETECT=true);
 COPY (
-    SELECT SITES.Site, SITES.Platform, HISTORY.* 
+    SELECT SITES.StationID, HISTORY.* 
     FROM SITES 
     JOIN HISTORY ON (
-        CONCAT(SITES.Site, '-', SITES.Platform) == HISTORY.locHistory_SiteCode
-         OR SITES.Site == HISTORY.locHistory_SiteCode
+        SITES.StationID == HISTORY.locHistory_SiteCode
+         OR SITES.StationID == HISTORY.locHistory_SiteCode
     ) 
 ) TO './build/fdri_asset_loc_history.csv';
